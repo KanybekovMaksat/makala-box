2b4d1f7b4bdab8c49c91e6d437b25fc5a725a9e2
KanybekovMaksat <maksatkanybekov2004@gmail.com>
2024-03-29 22:35:21 +0600

    initialized project: general configure setting enviroment, install labrary, configure alias, set folder structure fsd methodology

---
+++ .eslintrc.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [
+          ['~app', './src/app'],
+          ['~entities', './src/entities'],
+          ['~features', './src/features'],
+          ['~pages', './src/pages'],
+          ['~shared', './src/shared'],
+          ['~widgets', './src/widgets'],
+        ],
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+      },
+    },
+  },
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+};
---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  preview: { open: true },
+  resolve: {
+    alias: {
+      '~app': path.resolve('src/app'),
+      '~entities': path.resolve('src/entities'),
+      '~features': path.resolve('src/features'),
+      '~pages': path.resolve('src/pages'),
+      '~shared': path.resolve('src/shared'),
+      '~widgets': path.resolve('src/widgets'),
+    },
+  },
+});
