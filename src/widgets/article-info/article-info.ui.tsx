dbde7c6386fda866ce7743a3d07ff73e2f0145f4
KanybekovMaksat <maksatkanybekov2004@gmail.com>
2024-05-01 22:22:26 +0600

    feat: created article info block for article page

--- src/widgets/article-info/article-info.ui.tsx
+++ src/widgets/article-info/article-info.ui.tsx
@@ -1,68 +1,59 @@
-import Avatar from '@mui/material/Avatar';
 import AccessTimeFilledIcon from '@mui/icons-material/AccessTimeFilled';
 import VisibilityIcon from '@mui/icons-material/Visibility';
-import { Chip,  Tooltip } from '@mui/material';
+import { Chip, Tooltip } from '@mui/material';
+import { articleTypes } from '~entities/article';
+import dayjs from 'dayjs';
+import 'dayjs/locale/ru';
+dayjs.locale('ru');
 
-export function ArticleInfo() {
+type ArticleInfoProps = { article: articleTypes.Article };
+
+export function ArticleInfo(props: ArticleInfoProps) {
   return (
-    <div className="flex gap-3 py-10">
-      <div className="max-w-[60%]">
-        <AvatarCard />
-        <h1 className="my-4 text-[30px] font-bold leading-9 font-extrabold">
-          Как дизайнеру и любителю технологий жить после Apple Vision Pro
+    <div className="flex flex-col-reverse md:flex-row gap-3 py-6">
+      <div className="md:min-w-[55%] md:max-w-[55%]">
+        <div className="flex items-center gap-2">
+          <div className="flex items-center gap-1 hover:cursor-pointer">
+            <p className="text-md font-bold hover:text-second-100">
+              {props.article.author}
+            </p>
+          </div>
+          <p className="text-md text-pc-400 text-sm ">
+            {dayjs(props.article.created).format('DD.MM.YYYY').toUpperCase()}
+          </p>
+          <div className="flex gap-3">
+            <p className="text-md text-pc-400 flex items-center gap-1 text-sm">
+              <VisibilityIcon className="w-5" />
+              {props.article.viewCount}
+            </p>
+            <Tooltip title="Время чтения">
+              <p className="text-md text-pc-400 flex items-center gap-1 text-sm">
+                <AccessTimeFilledIcon className="w-4" />
+                {props.article.readTime} мин
+              </p>
+            </Tooltip>
+          </div>
+        </div>
+        <h1 className="max-w-[100%] break-words my-2 text-[30px] font-bold leading-7">
+          {props.article.title}
         </h1>
         <div className="flex gap-1 my-3">
-          <Chip
-            label="Программирование"
-            size="small"
-            variant="outlined"
-            className="text-pc-400 font-bold"
-          />
-          <Chip
-            label="Дизайн "
-            size="small"
-            variant="outlined"
-            className="text-pc-400 font-bold"
-          />
-          <Chip
-            label="Советы "
-            size="small"
-            variant="outlined"
-            className="text-pc-400 font-bold"
-          />
+          {props.article.categories.map((category, index) => (
+            <Chip
+              key={index}
+              label={category}
+              size="small"
+              variant="outlined"
+              className="text-second-100 border-second-100 font-medium  rounded"
+            />
+          ))}
         </div>
       </div>
       <img
-        className="h-[250px] w-[40%] object-cover rounded"
-        src="https://i.pinimg.com/564x/b4/1a/8a/b41a8acccf85813efcddf1d93061ecc6.jpg"
+        className="min-h-[300px] max-h-[320px] min-w-[45%] max-w[45%] object-cover rounded"
+        src={props.article.photo}
         alt=""
       />
-    </div>
-  );
-}
-
-function AvatarCard() {
-  return (
-    <div className="flex items-center gap-3">
-      <div className="flex items-center gap-1 hover:cursor-pointer">
-        <Avatar
-          alt="Maksat"
-          src="https://comtehno.kg/wp-content/uploads/2019/11/cropped-cropped-1789_oooo.plus_-2-300x300.png"
-        />
-        <p className="text-md font-bold hover:text-[green]">Максат Каныбеков</p>
-      </div>
-      <p className="text-md text-pc-400 text-sm hidden md:block">
-        Апрель 2, 2024
-      </p>
-      <p className="text-md text-pc-400 flex items-center gap-1 text-sm">
-        <VisibilityIcon className="w-5" />
-        566
-      </p>
-      <Tooltip title="Время чтения">
-        <p className="text-md text-pc-400 flex items-center gap-1 text-sm">
-          <AccessTimeFilledIcon className="w-4" />5 мин
-        </p>
-      </Tooltip>
     </div>
   );
 }
